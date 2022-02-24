945203392d0e5f83471b5e2134300976fe90369e
Daniel Kiessling <dkiessling@mailbox.org>
2022-02-25 00:31:29 +0100

    add project structure

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fluffy_split_scratch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fluffy!");
+        }
+    }
+}
